f71fe3a83aa0c375e91b5383aecc76677549c692
Johnny Hong <johnny_hong0116@icloud.com>
2025-10-28 22:56:41 -0500

    feat(DetailedItems): Milestone 1 add detailed items screen

--- src/features/marketplace/screens/ItemDetailScreen.tsx
+++ src/features/marketplace/screens/ItemDetailScreen.tsx
@@ -173,6 +173,7 @@ export default function ItemDetailScreen() {
   );
 }
 
+{/* Styles */}
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: COLORS.background },
   imageContainer: {
